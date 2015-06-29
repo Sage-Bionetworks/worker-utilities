73ce65fc60010f08fc79feb681d3b13ba33f1379
john-hill <john.hill@sagebase.org>
2015-06-29 14:47:30 -0700

    ready

--- src/main/java/org/sagebionetworks/workers/util/aws/message/MessageDrivenWorkerStackConfiguration.java
+++ src/main/java/org/sagebionetworks/workers/util/aws/message/MessageDrivenWorkerStackConfiguration.java
@@ -107,5 +107,35 @@ public class MessageDrivenWorkerStackConfiguration {
 	public void setTopicNamesToSubscribe(List<String> topicNamesToSubscribe){
 		messageQueueConfiguration.setTopicNamesToSubscribe(topicNamesToSubscribe);
 	}
+	
+	/**
+	 * Optional parameter used to configure this queue to use setup a dead
+	 * letter queue for failed messages.
+	 * 
+	 * If this is set, then {@link #setDeadLetterMaxFailureCount(Integer)} must also be
+	 * set.
+	 * 
+	 * @param deadLetterQueueName
+	 *            The name of the dead letter queue where failed messages should
+	 *            be pushed when the max failure count is exceeded.
+	 */
+	public void setDeadLetterQueueName(String deadLetterQueueName) {
+		messageQueueConfiguration.setDeadLetterQueueName(deadLetterQueueName);
+	}
+	
+	/**
+	 * An optional parameter used to configure this queue to forward failed
+	 * messages to a dead letter queue.
+	 * 
+	 * If this is set then the {@link #setDeadLetterQueueName(Integer)} must
+	 * also be set.
+	 * 
+	 * @param maxFailureCount
+	 *            The maximum number of times a message should be retried before
+	 *            before being pushed to the dead letter queue.
+	 */
+	public void setDeadLetterMaxFailureCount(Integer maxFailureCount) {
+		messageQueueConfiguration.setMaxFailureCount(maxFailureCount);
+	}
 
 }
