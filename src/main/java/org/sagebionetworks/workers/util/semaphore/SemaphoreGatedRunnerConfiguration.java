deb96152fd79cb082687f7a7a79fa2c60fd87b7a
john-hill <john.hill@sagebase.org>
2015-07-24 17:22:01 -0700

    semaphore gated runner will forward progress events

--- src/main/java/org/sagebionetworks/workers/util/semaphore/SemaphoreGatedRunnerConfiguration.java
+++ src/main/java/org/sagebionetworks/workers/util/semaphore/SemaphoreGatedRunnerConfiguration.java
@@ -12,6 +12,7 @@ import org.sagebionetworks.workers.util.progress.ProgressingRunner;
 public class SemaphoreGatedRunnerConfiguration<T> {
 
 	ProgressingRunner<T> runner;
+	ProgressCallback<T> progressCallack;
 	String lockKey;
 	long lockTimeoutSec = -1;
 	int maxLockCount = -1;
@@ -40,11 +41,39 @@ public class SemaphoreGatedRunnerConfiguration<T> {
 	 */
 	public SemaphoreGatedRunnerConfiguration(ProgressingRunner<T> runner,
 			String lockKey, long lockTimeoutSec, int maxLockCount) {
+		this(runner, lockKey, lockTimeoutSec, maxLockCount, null);
+	}
+
+	/**
+	 * 
+	 * @param runner
+	 *            The runner that will be called when a lock is acquired. The
+	 *            lock will be held for the duration of this runners run()
+	 *            method.
+	 * @param lockKey
+	 *            The semaphore lock key that must be held in order to run the
+	 *            runner.
+	 * @param lockTimeoutSec
+	 *            The timeout of the semaphore lock in seconds. The runner must
+	 *            either terminate before this timeout expires or call
+	 *            {@link ProgressCallback#progressMade()} to extend the timeout.
+	 * @param maxLockCount
+	 *            The maximum number of concurrent locks that can be issued for
+	 *            the given semaphore key. If the runner is expected to be a
+	 *            singleton, then set this value to one.
+	 * @param progressCallback
+	 *            An optional parameter. When provided, progress made events
+	 *            will be forwarded to this callback as well.
+	 */
+	public SemaphoreGatedRunnerConfiguration(ProgressingRunner<T> runner,
+			String lockKey, long lockTimeoutSec, int maxLockCount,
+			ProgressCallback<T> progressCallback) {
 		super();
 		this.runner = runner;
 		this.lockKey = lockKey;
 		this.lockTimeoutSec = lockTimeoutSec;
 		this.maxLockCount = maxLockCount;
+		this.progressCallack = progressCallback;
 	}
 
 	/**
@@ -127,6 +156,26 @@ public class SemaphoreGatedRunnerConfiguration<T> {
 	 */
 	public void setMaxLockCount(int maxLockCount) {
 		this.maxLockCount = maxLockCount;
+	}
+
+	/**
+	 * An optional parameter. When provided, progress made events will be
+	 * forwarded to this callback as well.
+	 * 
+	 * @return
+	 */
+	public ProgressCallback<T> getProgressCallack() {
+		return progressCallack;
+	}
+
+	/**
+	 * An optional parameter. When provided, progress made events will be
+	 * forwarded to this callback as well.
+	 * 
+	 * @param progressCallack
+	 */
+	public void setProgressCallack(ProgressCallback<T> progressCallack) {
+		this.progressCallack = progressCallack;
 	}
 
 }
